c96f99108a548b3e7f8b03fb22007bb497eed1a8
Prestoken <31255245+Prestoken@users.noreply.github.com>
2018-12-16 14:53:17 +0100

    Initial commit

---
+++ src/config/Ascii.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018, The AZUR Developers, The TURTLECOIN Developers
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string windowsAsciiArt ="";
+
+const std::string nonWindowsAsciiArt = "";
+
+/* Windows has some characters it won't display in a terminal. If your ascii
+   art works fine on Windows and Linux terminals, just replace 'asciiArt' with
+   the art itself, and remove these two #ifdefs and above ascii arts */
+#ifdef _WIN32
+const std::string asciiArt = windowsAsciiArt;
+#else
+const std::string asciiArt = nonWindowsAsciiArt;
+#endif
---
+++ src/config/CryptoNoteConfig.h
@@ -0,0 +1,171 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2018, The AZUR Developers, The TURTLECOIN Developers
+//
+// Please see the included LICENSE file for more information.
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <limits>
+#include <initializer_list>
+#include <boost/uuid/uuid.hpp>
+
+namespace CryptoNote {
+namespace parameters {
+
+const uint64_t DIFFICULTY_TARGET                             = 7;
+const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
+const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 18;
+const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 1;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3         = 3 * DIFFICULTY_TARGET;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V4         = 6 * DIFFICULTY_TARGET;
+const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
+const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 60;
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(100000000000);
+const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 0;
+const size_t   ZAWY_DIFFICULTY_V2                            = 0;
+const uint8_t  ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION      = 3;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 1;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V2              = 2;
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3              = 3;
+const uint64_t LWMA_3_DIFFICULTY_BLOCK_INDEX                 = 4;
+const unsigned EMISSION_SPEED_FACTOR                         = 25;
+static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
+const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(25000000000);
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010101ff000180f4f6905d026d40fbdda072310fead23e0043ed52b7c94f3bbcb2661ce78bc1fd969b6994cd210114196c177a324a66c5b92d6f30f69048914fcaf037a4aab44f9acb5d879ef251";
+const uint64_t GENESIS_BLOCK_TIMESTAMP                       = 1544716228;
+const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 100000;
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2  = 20000;
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1  = 10000;
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE;
+const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
+const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 2;
+const uint64_t MINIMUM_FEE                                   = UINT64_C(01);
+const uint64_t MINIMUM_MIXIN_V1                              = 0;
+const uint64_t MAXIMUM_MIXIN_V1                              = 10;
+const uint64_t MINIMUM_MIXIN_V2                              = 1;
+const uint64_t MAXIMUM_MIXIN_V2                              = 10;
+const uint64_t MINIMUM_MIXIN_V3                              = 2;
+const uint64_t MAXIMUM_MIXIN_V3                              = 10;
+const uint32_t MIXIN_LIMITS_V1_HEIGHT                        = 1;
+const uint32_t MIXIN_LIMITS_V2_HEIGHT                        = 100000;
+const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 5000000;
+const uint64_t DEFAULT_MIXIN_V0                              = 0;
+const uint64_t DEFAULT_MIXIN_V1                              = MINIMUM_MIXIN_V1;
+const uint64_t DEFAULT_MIXIN_V2                              = MINIMUM_MIXIN_V2;
+const uint64_t DEFAULT_MIXIN_V3                              = MINIMUM_MIXIN_V3;
+
+const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(01);
+const uint64_t DEFAULT_DUST_THRESHOLD_V2                     = UINT64_C(01);
+
+const uint32_t DUST_THRESHOLD_V2_HEIGHT                      = MIXIN_LIMITS_V2_HEIGHT;
+const uint32_t FUSION_DUST_THRESHOLD_HEIGHT_V2               = 800000;
+const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
+
+const size_t   DIFFICULTY_WINDOW                             = 60;
+const size_t   DIFFICULTY_WINDOW_V1                          = 60;
+const size_t   DIFFICULTY_WINDOW_V2                          = 60;
+const uint64_t DIFFICULTY_WINDOW_V3                          = 60;
+const uint64_t DIFFICULTY_BLOCKS_COUNT_V3                    = DIFFICULTY_WINDOW_V3 + 1;
+
+const size_t   DIFFICULTY_CUT                                = 15;
+const size_t   DIFFICULTY_CUT_V1                             = 15;
+const size_t   DIFFICULTY_CUT_V2                             = 15;
+const size_t   DIFFICULTY_LAG                                = 15;
+const size_t   DIFFICULTY_LAG_V1                             = 15;
+const size_t   DIFFICULTY_LAG_V2                             = 15;
+static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
+const size_t   MAX_BLOCK_SIZE_INITIAL                        = 100000;
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         = 100 * 1024;
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 60 / DIFFICULTY_TARGET;
+const uint64_t MAX_EXTRA_SIZE                                = 140000;
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
+const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;   
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7;
+const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7;  
+const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT * 30 / 100;
+const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
+const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
+
+const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
+
+const uint32_t UPGRADE_HEIGHT_V2                             = 1;
+const uint32_t UPGRADE_HEIGHT_V3                             = 2;
+const uint32_t UPGRADE_HEIGHT_V4                             = 3;
+const uint32_t UPGRADE_HEIGHT_CURRENT                        = UPGRADE_HEIGHT_V4;
+const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
+const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
+const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
+const uint64_t FORK_HEIGHTS[] =
+{   200000,  // 0
+    300000,  // 1
+    400000,  // 2
+    600000,  // 3
+};
+const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 6;
+const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
+const uint8_t CURRENT_FORK_INDEX = FORK_HEIGHTS_SIZE == 0 ? 0 : SOFTWARE_SUPPORTED_FORK_INDEX;
+const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.bin";
+const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.bin";
+const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
+const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
+const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
+}
+const char     CRYPTONOTE_NAME[]                             = "Azur";
+const uint8_t  TRANSACTION_VERSION_1                         =  1;
+const uint8_t  TRANSACTION_VERSION_2                         =  2;
+const uint8_t  CURRENT_TRANSACTION_VERSION                   =  TRANSACTION_VERSION_1;
+const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
+const uint8_t  BLOCK_MAJOR_VERSION_2                         =  2;
+const uint8_t  BLOCK_MAJOR_VERSION_3                         =  3;
+const uint8_t  BLOCK_MAJOR_VERSION_4                         =  4;
+const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
+const uint8_t  BLOCK_MINOR_VERSION_1                         =  1;
+const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by default, blocks ids count in synchronizing
+const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
+const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
+const int      P2P_DEFAULT_PORT                              =  15250;
+const int      RPC_DEFAULT_PORT                              =  15251;
+const int      SERVICE_DEFAULT_PORT                          =  8070;
+const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
+const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
+const uint8_t  P2P_CURRENT_VERSION                           = 3;
+const uint8_t  P2P_MINIMUM_VERSION                           = 2;
+const uint8_t  P2P_UPGRADE_WINDOW                            = 2;
+const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 32 * 1024 * 1024; // 32 MB
+const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
+const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
+const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;            // seconds
+const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;      // 50000000 bytes maximum packet size
+const uint32_t P2P_DEFAULT_PEERS_IN_HANDSHAKE                = 250;
+const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;          // 5 seconds
+const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;          // 2 seconds
+const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; // 2 minutes
+const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
+const uint64_t DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE         = 256;
+const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE          = 10;
+const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES               = 100;
+const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT     = 2;
+const char     LATEST_VERSION_URL[]                          = "http://latest.getazur.org";
+const std::string LICENSE_URL                                = "https://github.com/getazur/azur/blob/master/LICENSE";
+const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
+{
+    {  0xb6, 0x1c, 0x4e, 0x14, 0xcf, 0x42, 0x24, 0x41, 0x51, 0xf9, 0x92, 0xb3, 0xb5, 0xc4, 0xc4, 0xc8  }
+};
+
+const char* const SEED_NODES[] = {
+  "node-1.getazur.org:15250",
+  "node-2.getazur.org:15250",
+  "node-3.getazur.org:15250",
+  "node-4.getazur.org:15250"
+};
+} // CryptoNote
