2de52294c9b2f766a1ad26ffb0b1f469e61b45cd
getazur <45802475+getazur@users.noreply.github.com>
2018-12-18 04:36:59 +0100

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -26,7 +26,7 @@ namespace WalletConfig
     const std::string daemonName = "azurd";
 
     /* The name to call this wallet */
-    const std::string walletName = "zedwallet";
+    const std::string walletName = "wallet";
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
