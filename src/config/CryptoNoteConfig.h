9881953e5845f1abc8ce46947a79b146c3edba78
Azur Network <45802475+getazur@users.noreply.github.com>
2019-02-03 14:46:38 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -163,7 +163,7 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 };
 
 const char* const SEED_NODES[] = {
-  "daemon-1.getazur.org:15250",
-  "daemon-2.getazur.org:15250"
+  "private.getazur.org:15250",
+  "private1.getazur.org:15250"
 };
 } // CryptoNote
